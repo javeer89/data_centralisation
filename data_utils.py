34cbbda3ebbbf4f6e2c640d05d8c847f545297e2
Javeer Miah <javeermiah@gmail.com>
2023-11-30 15:46:50 +0000

    updated data_utils with a __name__ == '__main__'

--- data_utils.py
+++ data_utils.py
@@ -5,6 +5,7 @@ import  yaml
 
 from    sqlalchemy import create_engine
 from    sqlalchemy import inspect
+from    sqlalchemy import text
 
 #CLASS Data Connector
 class   DataConnector:
@@ -26,52 +27,16 @@ class   DataConnector:
         return inspector.get_table_names()
 
 
-
-#%%
-
-import  yaml
-def read_db_creds(name):
-    with open(name, 'r') as stream:
-        return yaml.safe_load(stream)
-
-db = "db_creds.yaml"
-read_db_creds(db)
-
-#progress notes: so we will need a way to define "name" object later on in code.
-
-#%%
-
-import psycopg2
-with psycopg2.connect(host='data-handling-project-readonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com', user='aicore_admin', password='AiCore2022', dbname='postgres', port=5432) as conn:
-    with conn.cursor() as cur:
-        cur.execute("""SELECT table_name FROM information_schema.tables
-       WHERE table_schema = 'public'""")
-        for table in cur.fetchall():
-            print(table)
-
-#used psycopg2 to view table for any needed information. none needed as of yet.
-
-#%%
-import yaml
-from sqlalchemy import create_engine
-from sqlalchemy import inspect
-import pandas as pd
-
-def read_db_creds(name):
-    with open(name, 'r') as stream:
-        return yaml.safe_load(stream)
-
-db = "db_creds.yaml"
-credentials = read_db_creds(db)
-rdbkey = read_db_creds(db).keys()
-rdbvalues = read_db_creds(db).values()
-
-print(list(rdbkey)[2])
-print(list(rdbvalues)[1])
-
-#engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-
-engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{credentials['RDS_USER']}:{credentials['RDS_PASSWORD']}@{credentials['RDS_HOST']}:{credentials['RDS_PORT']}/{credentials['RDS_DATABASE']}")     
-
-inspector = inspect(engine)
-inspector.get_table_names()
+if __name__ == '__main__':
+    dc = DataConnector()
+    engine = dc.init_db_engine()
+    engine.connect()
+    print("Hi") 
+    print(engine)
+    dbt = dc.list_db_tables(engine)
+    print(dbt)
+    with engine.connect() as connection:
+        result = connection.execute(text())
+        for row in result:
+            print(row)
+            
